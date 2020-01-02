27220eab6c1a253a23737bedabd1815132347da6
Sushila Goyat <sush.rinwa@gmail.com>
2020-01-01 20:35:59 -0700

    operatrors

---
+++ src/operators.js
@@ -0,0 +1,17 @@
+let x=2;let y=4;
+let z='2';
+//console.log(x+y);
+//console.log(x++);//post incrrement
+//console.log(++x);//pre increment
+//console.log(x*y);
+//=== is  check value as well as type 
+console.log(x==y);// == checks value on both sides(value check)
+console.log(x==z);
+console.log(x===z);//==== checks type as well as value(type and value check)
+console.log(x > y ? x : y);
+
+let a=true;
+let b=false;
+console.log(a && b);
+console.log(a || b);
+console.log( !a );
---
+++ src/operators1.js
@@ -0,0 +1,18 @@
+let x=10
+let y='string';
+let z=true;
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+
+console.log(y instanceof String);
+y=new String();
+console.log(y instanceof String);
+
+console.log(typeof 12345);
+console.log(typeof "My");
+console.log(typeof false);
+console.log(typeof NaN);
+console.log(typeof {name:'y'});
+console.log(typeof [1,2,3]);
