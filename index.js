e2682ac4170e745c2b2124e819ed267a0d1b753f
Tech231082 <sush.rinwa@gmail.com>
2020-01-14 21:39:39 -0700

    practice

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ for(let i=0;i<4;i++){
 }
 let age=22;
 console.log(name+' '+age);
+const s='3009';
+console.log(s);
+let ob=null;
+let employee={name:"Peter",job:"HR"};
+console.log(employee.name);
+console.log(employee.job);
+console.log(employee);
+employee['name']="Beth";
+console.log(employee.name);
+let arr=[1,2,2,3,4,5,6,9,0];
+console.log(arr.length);
+
+function getName(name){
+  return name;
+}
+console.log(getName('jivi'));
